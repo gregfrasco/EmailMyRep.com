ddb83bd079ccd6ae4b618e62a7b77298fc07125b
Greg Frasco <frascog@wit.edu>
2016-02-16 11:37:09 -0500

    Added Views

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET topic page. */
+router.get('/topics', function(req, res, next) {
+  res.render('topics', { title: 'topics' });
+});
+/* GET template page. */
+router.get('/template', function(req, res, next) {
+  res.render('template', { title: 'template' });
+});
+/* GET email page. */
+router.get('/email', function(req, res, next) {
+  res.render('email', { title: 'email' });
+});
 
 module.exports = router;
