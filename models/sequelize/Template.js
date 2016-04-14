18af2acf67bb75650322f6373cd24dda2f88d38f
Greg Frasco <frascog@wit.edu>
2016-04-14 12:03:11 -0400

    Increase Length to Templates

--- models/sequelize/Template.js
+++ models/sequelize/Template.js
@@ -8,7 +8,7 @@ module.exports = function(db, DataTypes) {
       primaryKey: true
     },
     title: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(10000),
       allowNull: false
     },
     description: {
